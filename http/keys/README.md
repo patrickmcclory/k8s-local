7b43bd5e07f41fac6a7fb8867abb134ca967ff8a
Patrick McClory <patrick@mcclory.io>
2016-11-06 13:26:26 -0800

    adding more stuff

---
+++ http/keys/README.md
@@ -0,0 +1,3 @@
+# Keys
+
+We're setting up a dev cluster... we'll make key management easy. In this directory we'll create all the keys we need to via the ```rpi-setup.sh``` script in the directory above this one. 
---
+++ rpi-setup.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+# Variables set per https://coreos.com/kubernetes/docs/latest/openssl.html
+MASTER_HOST=172.16.16.10
+K8S_SERVICE_IP=10.3.0.1
+CLUSTER_DOMAIN_NAME=lab.mcclory.io
+NUMBER_OF_HOSTS=7
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sed "/DNSMASQ_OPTS=/c\DNSMASQ_OPTS=$DIR\/dnsmasq\/dnsmasq.conf" /etc/default/dnsmasq
+
+if [ -d /var/lib/tftpboot/pxelinux.cfg ];
+then
+  sudo rm -rf /var/lib/tftpboot/pxelinux.cfg
+fi
+
+sudo mkdir -p /var/lib/tftpboot/pxelinux.cfg
+
+# setting up tftp boot folder
+sudo mkdir -p /var/lib/tftpboot/pxelinux.cfg
+
+ln -s $(pwd)/tftpboot/pxelinux.cfg /var/lib/tftpboot/pxelinux
+
+echo ''
+echo 'Getting CoreOS files for alpha, beta and stable releases... just in case'
+echo ''
+
+sudo mkdir -p /var/lib/tftpboot/coreos/alpha
+sudo mkdir -p /var/lib/tftpboot/coreos/beta
+sudo mkdir -p /var/lib/tftpboot/coreos/stable
+
+for releasename in alpha beta stable; do
+  sudo mkdir -p /var/lib/tftpboot/coreos/$releasename
+  curl -O /var/lib/tftpboot/coreos/$releasename/ https://$releasename.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz
+  curl -O /var/lib/tftpboot/coreos/$releasename/ https://$releasename.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz
+done
+
+echo ''
+echo 'Getting core pxeboot files for pxeboot process'
+echo ''
+
+for filename in gpxelinux.0 ldlinux.c32 lpxelinux.0 memdisk menu.c32 pxelinux.0 do
+  rm -rf /var/lib/tftpboot/$filename
+  curl -O /var/lib/tftpboot/ http://www.mcclory.io/resources/pxeboot/$filename
+done
+
+# Set up nginx and configure for use
+
+if [ $(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get install -y nginx;
+fi
+
+if [ -f /etc/nginx/sites-enabled/default ];
+then
+  rm -rf /etc/nginx/sites-enabled/default
+fi
+
+cat << /etc/nginx/sites-availale/pxeboot.conf >> EOF
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+  root $DIR/http;
+  server_name _;
+  location / {
+    try_files $uri $uri/ =404;
+  }
+
+EOF
+
+# Download k8s files
+sudo mkdir -p $DIR/http/k8s/1.4.5
+curl -L https://github.com/kubernetes/kubernetes/releases/download/v1.4.5/kubernetes.tar.gz -O /tmp/kuberntes.tar.gz
+cd /tmp
+tar xvf /tmp/kuberntes.tar.gz
+mv /tmp/kubernetes/server/kubernetes-server-linux-amd64.tar.gz $DIR/http/k8s/1.4.5/
+rm -rf /tmp/kubernetes
+rm -rf /tmp/kubernetes.tar.gz
+
+
+# Create us some keys!
+# Basically following steps here: https://coreos.com/kubernetes/docs/latest/openssl.html
+
+cd $DIR/http/keys
+
+echo ""
+echo "Creating Clicster Root CA"
+echo ""
+
+openssl genrsa -out ca-key.pem 2048
+openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj "/CN=kube-ca"
+
+echo ""
+echo "Creating API Server Keypair"
+echo ""
+
+cat >> api-openssl.cnf << EOF
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = kubernetes
+DNS.2 = kubernetes.default
+DNS.3 = kubernetes.default.svc
+DNS.4 = kubernetes.default.svc.cluster.local
+DNS.5 = ${CLUSTER_DOMAIN_NAME}
+IP.1 = ${K8S_SERVICE_IP}
+IP.2 = ${MASTER_HOST}
+
+EOF
+
+openssl genrsa -out apiserver-key.pem 2048
+openssl req -new -key apiserver-key.pem -out apiserver.csr -subj "/CN=kube-apiserver" -config api-openssl.cnf
+openssl x509 -req -in apiserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out apiserver.pem -days 10000 -extensions v3_req -extfile api-openssl.cnf
+
+echo ""
+echo "Creating Worker Keypairs"
+echo ""
+
+cat >> worker-openssl.cnf << EOF
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+[alt_names]
+IP.1 = \$ENV::WORKER_IP
+
+EOF
+
+for (( i=1; i<=$NUMBER_OF_HOSTS; i++ ))
+do
+
+FMT_DIGIT=$(printf "%02d" $i)
+
+WORKER_FQDN=dev-${FMT_DIGIT}.$CLUSTER_DOMAIN_NAME
+WORKER_IP=192.168.1.${i}0
+
+echo "FQDN: "$WORKER_FQDN
+echo "IP:   "$WORKER_IP
+echo ""
+
+openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
+WORKER_IP=${WORKER_IP} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config worker-openssl.cnf
+WORKER_IP=${WORKER_IP} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 10000 -extensions v3_req -extfile worker-openssl.cnf
+
+echo ""
+echo "done!"
+echo ""
+echo ""
+done
+echo "--------------------------------------------------------------------------------"
+
+sudo chmod 600 $DIR/http/keys/*
