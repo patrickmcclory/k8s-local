189cb5f69c63ef357ff4d0f5cb1bc20aa629f0d6
Patrick McClory <pmcclory@gmail.com>
2016-12-08 16:39:32 -0800

    fixing waiters

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+while (( $(kubectl get nodes | wc -l) < 4 )); do
+  echo 'Waiting for Kubernetes to become available';
+  sleep 10;
+done;
+
+kubectl create -f "${DIR}/deploy/kube-dns-addon.yaml"
+kubectl create -f "${DIR}/deploy/kubernetes-dashboard.yaml"
+
+echo "********************************************************************************"
+echo "*"
+echo "* Done Deploying assets into Kubernetes"
+echo "*"
+echo "********************************************************************************"
